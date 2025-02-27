f024c5f53d9ca8297a723eccc05df0af9b4cc851
ernest-kaminski <kaminski.ern@gmail.com>
2025-02-27 18:48:43 +0100

    cd local

---
+++ src/types/global.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace NodeJS {
+      interface Global {
+        mongoose: {
+          conn: any;
+          promise: Promise<any> | null;
+        };
+      }
+    }
+  }
+  
+export {};
+  
